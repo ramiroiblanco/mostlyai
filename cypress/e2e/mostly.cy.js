9e5f063325aee0401148e385bfd7b3575224ad35
Blanco, Ramiro [SET Pathfinder] <ramiro.blanco@oracle.com>
2022-06-21 19:25:14 +0200

    adds support functions and beforeEach

--- cypress/e2e/mostly.cy.js
+++ cypress/e2e/mostly.cy.js
@@ -1,23 +1,30 @@
 describe('Mostly AI', () => {
+  beforeEach(() => {
+    cy.visit('https://mostly.ai/')
+  })
+
   it('Verifies all bookmarks display on screen', () => {
     //Inconsistencies: Use cases vs Synthetic Data second word casing is not always the same.
-    const bookmarks = ['Platform', 'Use cases', 'Synthetic Data', 'Resources', 'Contact']
+    const strings = ['Platform', 'Use cases', 'Synthetic Data', 'Resources', 'Contact']
 
-    cy.visit('https://mostly.ai/')
-
-    bookmarks.forEach(bookmark => cy.contains(bookmark).should('be.visible'))
+    strings.forEach(string => verifyByText(string))
   })
 
   it('Verifies empty search results', () => {
-    //click search
-    cy.get('button[aria-label="Open search"]').click({force: true})
-    //enter search text
-    cy.get('.oxy-header-search_search-field').type('sythetic{enter}', {force: true})
+    const strings = ['Sorry, no results for', 'sythetic', '0 results found']
 
-    //verify search result
-    cy.contains('Sorry, no results for').should('be.visible')
-    cy.contains('sythetic').should('be.visible')
-    cy.contains('0 results found').should('be.visible')
+    doSearch('sythetic{enter}')
+
+    strings.forEach(string => verifyByText(string))
 
   })
+
+  function doSearch(text){
+    cy.get('button[aria-label="Open search"]').click({force: true})
+    cy.get('.oxy-header-search_search-field').type(text, {force: true})
+  }
+
+  function verifyByText(text) {
+    cy.contains(text).should('be.visible')
+  }
 })
