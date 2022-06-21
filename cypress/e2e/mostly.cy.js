fe4506b430099943db89fbd671392bd2bdb4ddf2
Blanco, Ramiro [SET Pathfinder] <ramiro.blanco@oracle.com>
2022-06-21 19:09:39 +0200

    adds Verifies empty search results

--- cypress/e2e/mostly.cy.js
+++ cypress/e2e/mostly.cy.js
@@ -5,6 +5,19 @@ describe('Mostly AI', () => {
 
     cy.visit('https://mostly.ai/')
 
-    bookmarks.forEach(bookmark => cy.contains(bookmark))
+    bookmarks.forEach(bookmark => cy.contains(bookmark).should('be.visible'))
+  })
+
+  it('Verifies empty search results', () => {
+    //click search
+    cy.get('button[aria-label="Open search"]').click({force: true})
+    //enter search text
+    cy.get('.oxy-header-search_search-field').type('sythetic{enter}', {force: true})
+
+    //verify search result
+    cy.contains('Sorry, no results for').should('be.visible')
+    cy.contains('sythetic').should('be.visible')
+    cy.contains('0 results found').should('be.visible')
+
   })
 })
