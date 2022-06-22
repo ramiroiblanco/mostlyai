2fed5929f34fbe5ecf94e8626f348b2cb29cf1dd
Blanco, Ramiro [SET Pathfinder] <ramiro.blanco@oracle.com>
2022-06-22 19:18:41 +0200

    Verifies form fields can be filled in

---
+++ cypress/e2e/data/formData.ts
@@ -0,0 +1,22 @@
+export const formData: {locator: string, value: string}[] = [
+     {
+        locator: `input[name="firstname"]`,
+        value: "Ramiro"
+    },
+     {
+        locator: `input[name="lastname"]`,
+        value: "Blanco"
+    },
+    {
+        locator: `input[name="email"][class="hs-input"]`,
+        value: "ramiro.blanco@mybusiness.com"
+    },
+    {
+        locator: `input[name="company"]`,
+        value: "Ramiro inc."
+    },
+    {
+        locator: `textarea[class="hs-input"]`,
+        value: `I'd like to know more about you!`
+    }
+]
--- cypress/e2e/mostly.cy.ts
+++ cypress/e2e/mostly.cy.ts
@@ -1,48 +1,61 @@
-describe('Mostly AI', () => {
+import {formData} from "./data/formData";
+
+describe("Mostly AI", () => {
   beforeEach(() => {
-    cy.visit('https://mostly.ai/')
+    cy.visit("https://mostly.ai/");
 
   })
 
-  it('Verifies all bookmarks display on screen', () => {
-    //Inconsistencies: Use cases vs Synthetic Data second word casing is not always the same.
-    const tabStrings = ['Platform', 'Use cases', 'Synthetic Data', 'Resources', 'Contact']
+  it("Verifies all bookmarks display on screen", () => {
+    const tabStrings = ["Platform", "Use cases", "Synthetic Data", "Resources", "Contact"];
 
     tabStrings.forEach(string => verifyElementByText(string))
   })
 
-  it('Verifies empty search results', () => {
-    const searchResultStrings = ['Sorry, no results for', 'sythetic', '0 results found']
-    const searchValue = 'sythetic{enter}'
+  it("Verifies empty search results", () => {
+    const searchResultStrings = ["Sorry, no results for", "sythetic", "0 results found"];
+    const searchValue = "sythetic{enter}";
 
-    doSearch(searchValue)
+    doSearch(searchValue);
 
-    searchResultStrings.forEach(string => verifyElementByText(string))
+    searchResultStrings.forEach(string => verifyElementByText(string));
   })
 
-  it.only('Verifies empty search results', () => {
-    cy.contains('Contact').click({force: true})
-    cy.get('#CookieBoxSaveButton').click()
-    cy.contains('Contact').click().click()
+  it("Verifies form fields can be filled in", () => {
+    const marketingCheckbox = `input[type="checkbox"][class="hs-input"]`;
+    const submitButton = `input[value="SEND MESSAGE"]`;
+    openContactForm();
 
-    cy.contains("We'll get back").click()
-    cy.get('input[name="firstname"]').type('Ramiro')
-    cy.get('input[name="lastname"]').type('Blanco')
-    cy.get('input[name="email"][class="hs-input"]').type('ramiroiblanco1980@mybusiness.com')
-    cy.get('input[name="company"]').type('Ramiro Inc.')
-    cy.get('textarea[class="hs-input"]').type("I'd like to know more about you!")
-    cy.get('input[type="checkbox"][class="hs-input"]').click()
+    formData.forEach(field => {
+      inputFormField(field.locator, field.value);
+    })
+
+    cy.get(marketingCheckbox).click();
+    cy.get(submitButton).trigger('mouseover');
   })
 })
 
 function doSearch(text: string): void {
-  const searchIcon = `button[aria-label="Open search"]`
-  const searchBox = ".oxy-header-search_search-field"
+  const searchIcon = `button[aria-label="Open search"]`;
+  const searchBox = ".oxy-header-search_search-field";
 
-  cy.get(searchIcon).click({force: true})
-  cy.get(searchBox).type(text, {force: true})
+  cy.get(searchIcon).click({force: true});
+  cy.get(searchBox).type(text, {force: true});
+}
+
+function inputFormField(locator: string, value: string){
+  cy.get(locator).type(value);
+}
+
+function openContactForm(): void {
+  cy.contains("Contact").click({force: true});
+  cy.get("#CookieBoxSaveButton").click();
+  cy.contains("Contact").click().click();
+
+  //Needed, if not, only spinner displays and form never loads. Maybe some kind of security to make sure a human is filling the form.
+  cy.contains("We'll get back").click();
 }
 
 function verifyElementByText(text: string): void {
-  cy.contains(text).should('be.visible')
+  cy.contains(text).should("be.visible");
 }
